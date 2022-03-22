482accb4ee0ebdcf79ad09b7b9491c36ee123bf4
septemblue <focusramxram@gmail.com>
2022-03-22 09:05:21 +0700

    get required extension and sign package

---
+++ app/src/main/java/com/septemblue/insorma/sign/SignActivity.java
@@ -0,0 +1,16 @@
+package com.septemblue.insorma.sign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.septemblue.insorma.R;
+
+public class SignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+    }
+}
